f48d7c5888cb231da17ab32e57d54f4a10db969d
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-13 16:57:03 +0530

    Add navbar!

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,9 +1,11 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 export default function AppLayout() {
   return (
     <div>
-        <Outlet />
+      <Header />
+      <Outlet />
     </div>
-  )
+  );
 }
