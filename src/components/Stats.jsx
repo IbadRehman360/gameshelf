3532bbce94c58c097d6916ac80383e4e3fb6aa7f
0xJordan <ahmedjamlan12@gmail.com>
2023-08-13 15:22:43 +0300

    Did responsiveness for stats across all devices

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,49 +1,125 @@
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+
 const Card = () => {
   return (
-    <div className="w-full md:w-1/2 lg:w-1/3">
-      <div className="p-4 rounded-lg shadow-md bg-white">
-        <div className="mb-4">
-          <h2 className="text-2xl sm:text-3xl  font-bold text-center text-gray-800 mb-3 ">
-            Discover the World of{" "}
-            <span className="text-orange-600">GameShelf</span>
-          </h2>
-
-          <div className="grid grid-cols-2 gap-4">
-            <div className="p-3 text-center border-r border-gray-300">
-              <p className="text-xl font-bold mb-2">Members</p>
-              <p className="text-lg font-medium text-blue-800">Join 839K+</p>
-              <p className="text-gray-700 font-light mt-2 text-sm">
+    <div className="max-w-7xl mx-auto pt-3 rounded-lg shadow-md bg-white">
+      <h2 className="text-lg sm:text-3xl font-bold text-center text-gray-800">
+        Discover the World of <span className="text-orange-600">GameShelf</span>
+      </h2>
+      <h3 className="text-xs sm:text-sm md:text-lg text-gray-600 text-center p-1">Explore gaming realms with GameShelf&apos;s guide</h3>
+      <span></span>
+      <div className="carousel md:grid md:grid-cols-2 xl:grid-cols-3 w-full">
+        <div
+          id="slide1"
+          className="carousel-item flex flex-col justify-center relative w-full"
+        >
+          <div className="flex justify-center py-2">
+            <div className="p-3 text-center border-r md:border-r-0 border-gray-300">
+              <p className="text-md font-bold mb-1">Members</p>
+              <p className="text-sm font-medium text-blue-800">Join 839K+</p>
+              <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
                 Join the gaming revolution. Explore thriving Buy & Sell
                 Marketplace.{" "}
               </p>
             </div>
-            <div className="p-3 text-center">
-              <p className="text-xl font-bold mb-2">Account</p>
-              <p className="text-lg font-semibold text-yellow-800">
+            <div className="p-3 relative text-center">
+              <div className="md:hidden absolute right-2 flex transform justify-center items-center  gap-1">
+                <a
+                  href="#slide2"
+                  className="border-[1px] border-gray-400 rounded-xl shadow-lg"
+                >
+                  <MdKeyboardArrowRight color="black" />
+                </a>
+              </div>
+              <p className="text-md font-bold mb-1">Account</p>
+              <p className="text-sm font-semibold text-yellow-800">
                 Explore 84K+
               </p>
-              <p className="text-gray-700 font-light mt-2 text-sm">
+            <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
                 Uncover unique in-game Accounts and elevate your new play
                 experience.
               </p>
             </div>
           </div>
         </div>
-        <div className="border-t pt-4 mb-2 ">
-          <div className="flex mb-1 ">
-            <p className="text-gray-500 font-semibold text-sm">
-              Elevate your gaming experience with GameShelfs exclusive
-              offerings.
-            </p>
-            <a
-              href="#"
-              className="px-4 py-2 sm:text-sm text-xs  bg-red-500 flex justify-center rounded-md items-center
-              text-white hover:bg-red-600 transition "
-              style={{ width: "50%" }}
-            >
-              Join Now
-            </a>
+        <div
+          id="slide2"
+          className="carousel-item flex flex-col justify-center relative w-full"
+        >
+          <div className="flex justify-center py-2">
+            <div className="p-3 text-center border-r md:border-r-0 border-gray-300">
+              <p className="text-md font-bold mb-1">Members</p>
+              <p className="text-sm font-medium text-blue-800">Join 839K+</p>
+            <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
+                Join the gaming revolution. Explore thriving Buy & Sell
+                Marketplace.{" "}
+              </p>
+            </div>
+            <div className="p-3 relative text-center">
+              <div className="md:hidden absolute right-2 flex transform justify-center items-center  gap-1">
+                <a
+                  href="#slide3"
+                  className="border-[1px] border-gray-400 rounded-xl shadow-lg"
+                >
+                  <MdKeyboardArrowRight color="black" />
+                </a>
+              </div>
+              <p className="text-md font-bold mb-1">Account</p>
+              <p className="text-sm font-semibold text-yellow-800">
+                Explore 84K+
+              </p>
+            <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
+                Uncover unique in-game Accounts and elevate your new play
+                experience.
+              </p>
+            </div>
           </div>
+        </div>
+        <div
+          id="slide3"
+          className="md:hidden xl:flex carousel-item flex flex-col justify-center relative w-full"
+        >
+          <div className="flex py-2">
+            <div className="p-3 relative text-center border-r md:border-r-0 border-gray-300">
+              <div className="md:hidden absolute left-2 flex transform justify-center items-center  gap-1">
+                <a
+                  href="#slide1"
+                  className="border-[1px] border-gray-400 rounded-xl shadow-lg"
+                >
+                  <MdKeyboardArrowLeft color="black" />
+                </a>
+              </div>
+              <p className="text-md font-bold mb-1">Members</p>
+              <p className="text-sm font-medium text-blue-800">Join 839K+</p>
+            <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
+                Join the gaming revolution. Explore thriving Buy & Sell
+                Marketplace.{" "}
+              </p>
+            </div>
+            <div className="p-3 text-center">
+              <p className="text-md font-bold mb-1">Account</p>
+              <p className="text-sm font-semibold text-yellow-800">
+                Explore 84K+
+              </p>
+            <p className="text-gray-700 font-light mt-1 text-xs md:text-base leading-5">
+                Uncover unique in-game Accounts and elevate your new play
+                experience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="border-t py-3 p-4">
+        <div className="flex items-center justify-center gap-2">
+          <p className="text-gray-500 font-normal text-[0.65rem]">
+            Elevate your gaming experience with GameShelfs exclusive offerings.
+          </p>
+          <a
+            href="#"
+            className="px-1 text-xs sm:text-sm  h-8 bg-red-500 rounded-md text-white hover:bg-red-600 transition w-24 flex justify-center items-center"
+          >
+            <span>Join Now</span>
+          </a>
         </div>
       </div>
     </div>
