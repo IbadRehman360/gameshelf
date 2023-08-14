615bbb14bc8b773efb5e35f65b81fd553a5be4d7
JakaiXu <105431008+JakaiXu@users.noreply.github.com>
2023-08-14 10:20:19 +1000

    fixed some bugs

--- src/components/TrendingGiftCard.jsx
+++ src/components/TrendingGiftCard.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 const TrendingGiftCard = () => {
+  const [currentItem, setCurrentItem] = useState(4);
   const [currentSlide, setCurrentSlide] = useState(0);
   const carouselItemClass = "carousel-item w-[48%] sm:w-[24%] flex flex-col";
   const carouselSpanClass =
@@ -13,54 +14,56 @@ const TrendingGiftCard = () => {
   const handleNextPage = () => {
     if (currentSlide < 2) setCurrentSlide(currentSlide + 1);
   };
-  const handleNextItem = () => {};
+  const handleNextItem = () => {
+    setCurrentItem(currentItem + 1);
+  };
   return (
     <div className="px-[10%]">
       <div className="flex items-center justify-between my-8">
         <h2 className="text-2xl font-bold">Trending Gift Cards</h2>
-        <div className="sm:w-[46px] sm:h-[46px] w-[24px] h-[24px] border sm:rounded-xl rounded-full flex items-center justify-center">
-          <img
-            src="/public/cheronRight.svg"
-            onClick={handleNextItem}
-            className="bg-white w-2"
-          />
-        </div>
+        <a
+          href={`#slide${currentItem}`}
+          onClick={handleNextItem}
+          className="sm:w-[46px] sm:h-[46px] w-[24px] h-[24px] border sm:rounded-xl rounded-full flex items-center justify-center cursor-pointer"
+        >
+          <img src="/public/cheronRight.svg" className="bg-white w-2" />
+        </a>
       </div>
       <div className="overflow-hidden">
         <div
           className="carousel rounded-lg gap-x-4 transition-transform duration-500 ease-in-out flex whitespace-nowrap"
           style={{ transform: `translateX(-${currentSlide * 100}%)` }}
         >
-          <div className={carouselItemClass}>
+          <div id="slide1" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard1.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Stem Wallet</span>
           </div>
 
-          <div className={carouselItemClass}>
+          <div id="slide2" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard2.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Apple Itunes</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide3" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard3.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Razor Gold</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide4" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard4.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Game Pass</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide5" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard1.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Stem Wallet</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide6" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard2.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Apple Itunes</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide7" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard3.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Razor Gold</span>
           </div>
-          <div className={carouselItemClass}>
+          <div id="slide8" className={carouselItemClass}>
             <img src="/public/giftcards/giftcard4.svg" alt="giftcardIcon" />
             <span className={carouselSpanClass}>Game Pass</span>
           </div>
--- src/components/TrendingVideoGames.jsx
+++ src/components/TrendingVideoGames.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 const TrendingVideoGames = () => {
+  const [currentVideo, setCurrentVideo] = useState(4);
   const [currentSlide, setCurrentSlide] = useState(0);
   const carouselItemClass = "carousel-item w-[48%] sm:w-[24%] flex flex-col";
-  const carouselSpanClass = "text-black mt-2 text-sm w-full text-center sm:hidden";
+  const carouselSpanClass =
+    "text-black mt-2 text-sm w-full text-center sm:hidden";
   const handlePrevPage = () => {
     if (currentSlide > 0) {
       setCurrentSlide(currentSlide - 1);
@@ -12,59 +14,61 @@ const TrendingVideoGames = () => {
   const handleNextPage = () => {
     if (currentSlide < 2) setCurrentSlide(currentSlide + 1);
   };
-  const handleNextItem = () => {};
+  const handleNextItem = () => {
+    setCurrentVideo(currentVideo + 1);
+  };
   return (
     <div className="px-[10%]">
       <div className="flex items-center justify-between my-8">
-        <h2 className="text-2xl font-bold">Trending Gift Cards</h2>
-        <div className="sm:w-[46px] sm:h-[46px] w-[24px] h-[24px] border sm:rounded-xl rounded-full flex items-center justify-center">
-          <img
-            src="/public/cheronRight.svg"
-            onClick={handleNextItem}
-            className="bg-white w-2"
-          />
-        </div>
+        <h2 className="text-2xl font-bold">Trending Video Games</h2>
+        <a
+          href={`#video${currentVideo}`}
+          onClick={handleNextItem}
+          className="sm:w-[46px] sm:h-[46px] w-[24px] h-[24px] border sm:rounded-xl rounded-full flex items-center justify-center"
+        >
+          <img src="/public/cheronRight.svg" className="bg-white w-2" />
+        </a>
       </div>
       <div className="overflow-hidden">
         <div
           className="carousel rounded-lg gap-x-4 transition-transform duration-500 ease-in-out flex whitespace-nowrap"
           style={{ transform: `translateX(-${currentSlide * 100}%)` }}
         >
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames1.svg" alt="giftcardIcon" />
+          <div id="video1" className={carouselItemClass}>
+            <img src="/public/videogames/videogames1.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>
               Assetto Corsa Steam CD Key
             </span>
           </div>
 
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames2.svg" alt="giftcardIcon" />
+          <div id="video2" className={carouselItemClass}>
+            <img src="/public/videogames/videogames2.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>WOW Prepaid Game Cards</span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames3.svg" alt="giftcardIcon" />
+          <div id="video3" className={carouselItemClass}>
+            <img src="/public/videogames/videogames3.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>Satisfactory PC - Steam</span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames4.svg" alt="giftcardIcon" />
+          <div id="video4" className={carouselItemClass}>
+            <img src="/public/videogames/videogames4.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>WOW Dragonflight</span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames4.svg" alt="giftcardIcon" />
+          <div id="video5" className={carouselItemClass}>
+            <img src="/public/videogames/videogames4.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>WOW Dragonflight</span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames2.svg" alt="giftcardIcon" />
+          <div id="video6" className={carouselItemClass}>
+            <img src="/public/videogames/videogames2.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>Dark Souls III PC - Steam</span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames1.svg" alt="giftcardIcon" />
+          <div id="video7" className={carouselItemClass}>
+            <img src="/public/videogames/videogames1.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>
               Assetto Corsa Steam CD Key
             </span>
           </div>
-          <div className={carouselItemClass}>
-            <img src="/public/videogames/videogames3.svg" alt="giftcardIcon" />
+          <div id="video8" className={carouselItemClass}>
+            <img src="/public/videogames/videogames3.svg" alt="videoGameIcon" />
             <span className={carouselSpanClass}>Satisfactory PC - Steam</span>
           </div>
         </div>
