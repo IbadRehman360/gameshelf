69f1a6377ecc8f4a6aae576da410c63bf07ed1f8
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-14 21:41:29 +0530

    Fix design issuses in features list.

--- src/components/Features/FeaturesItem.jsx
+++ src/components/Features/FeaturesItem.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FeaturesItem({ Details }) {
   const { icon, heading, description } = Details;
   return (
-    <article className='relative p-2 pb-8 flex flex-col items-center gap-2 text-center border-r-[2px] border-gray-200 even:pl-4 even:border-0 sm:even:border-r-[2px] sm:last-of-type:border-0'>
+    <article className='relative p-2 pb-8 flex flex-col items-center gap-2 text-center odd:border-r-[2px] border-gray-200 sm:border-r-[2px] sm:last-of-type:border-none'>
       {icon}
       <h5 className='font-semibold text-sm'>{heading}</h5>
       <p className='text-gray-400 text-sm font-extralight'>{description}</p>
