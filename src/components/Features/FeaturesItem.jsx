4c1812ca9ef96d9fb708a2fdf9e7f08fdf0a95f9
Imran Biswas <imranbiswas2209@gmail.com>
2023-08-15 09:53:43 +0530

    Fix features and payments section.

--- src/components/Features/FeaturesItem.jsx
+++ src/components/Features/FeaturesItem.jsx
@@ -3,11 +3,15 @@ import React from "react";
 export default function FeaturesItem({ Details }) {
   const { icon, heading, description } = Details;
   return (
-    <article className='relative p-2 pb-8 flex flex-col items-center gap-2 leading-loose tracking-wider text-center odd:border-r-[2px] border-gray-200 sm:border-r-[2px] sm:last-of-type:border-none sm:mb-0'>
+    <article className='relative mx-auto p-2 pb-8 flex flex-col items-center gap-2 leading-loose tracking-wider text-center  sm:border-gray-200 sm:border-r-[2px] sm:last-of-type:border-none sm:mb-0'>
       {icon}
-      <h5 className='font-semibold text-sm'>{heading}</h5>
-      <p className='text-gray-400 text-sm font-extralight'>{description}</p>
-      <div className='absolute mb-3 bottom-1 left-[37.5%] h-[3px] w-[25%] bg-black sm:mb-1 sm:block' />
+      <h5 className='font-semibold text-xs lg:text-sm  2xl:text-lg'>
+        {heading}
+      </h5>
+      <p className='text-gray-400 text-[0.65rem] lg:text-xs 2xl:text-sm font-extralight'>
+        {description}
+      </p>
+      <div className='absolute mb-3 bottom-1 left-[35%%] h-[3px] w-[30%] bg-black sm:left-[40%] sm:w-[20%] sm:mb-1 sm:block' />
     </article>
   );
 }
--- src/components/Features/FeaturesList.jsx
+++ src/components/Features/FeaturesList.jsx
@@ -4,35 +4,39 @@ import FeaturesItem from "./FeaturesItem";
 
 const FEATURES = [
   {
-    icon: <FaFeather className='text-5xl mb-4' />,
+    icon: <FaFeather className='text-3xl sm:text-5xl sm:mb-4' />,
     heading: "East to Use",
     description:
-      "A simple and useful interface that helps you to get things done",
+      "Streamlined, helpful interface aiding efficient task completion",
   },
   {
-    icon: <FaShieldAlt className='text-5xl mb-4' />,
+    icon: <FaShieldAlt className='text-3xl sm:text-5xl sm:mb-4' />,
     heading: "Totally Secure",
-    description:
-      "All payment methods and technologies consider the data security",
+    description: "All payment methods and tech prioritize data security",
   },
+  {},
   {
-    icon: <FaHandshake className='text-5xl mb-4' />,
+    icon: <FaHandshake className='text-3xl sm:text-5xl sm:mb-4' />,
     heading: "Customer Happiness",
-    description: "We are always here to help you after or before your purchase",
+    description: "Assistance available both pre- and post-purchase for you",
   },
   {
-    icon: <AiFillThunderbolt className='text-5xl mb-4' />,
-    heading: "Lorem, ipsum dolor.",
-    description: "Accounts that you buy will be delivered as soon as possible.",
+    icon: <AiFillThunderbolt className='text-3xl sm:text-5xl sm:mb-4' />,
+    heading: "Fastest Delivery",
+    description: "Purchased accounts promptly delivered with efficiency",
   },
 ];
 
 export default function FeaturesList() {
   return (
-    <section className='py-12 grid grid-cols-[repeat(2,_40%)] justify-center sm:grid-cols-[repeat(4,_22%)] lg:grid-cols-[repeat(4,_18%)]'>
-      {FEATURES.map((feature, index) => (
-        <FeaturesItem key={index} Details={feature} />
-      ))}
+    <section className='relative py-2 my-1 grid grid-cols-[repeat(2,_40%)] justify-around sm:my-4 lg:my-8 sm:justify-center sm:grid-cols-[repeat(4,_22%)] lg:grid-cols-[repeat(4,_18%)] 2xl:max-w-[80%] 2xl:mx-auto 2xl:my-6 '>
+      {FEATURES.map((feature, index) => {
+        return index != 2 ? (
+          <FeaturesItem key={index} Details={feature} />
+        ) : (
+          <div className='absolute w-[1px] h-full bg-gray-300 left-[50%] sm:hidden' />
+        );
+      })}
     </section>
   );
 }
