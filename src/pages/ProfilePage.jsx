9879ee26be513e3bcad868093f41fdb61a6c2e23
0xJordan <ahmedjamlan12@gmail.com>
2023-08-20 23:06:22 +0300

    Created Profile Page

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import ProfileCard from "../components/ProfileCard";
+import ProfileItems from "../components/ProfileItems";
+
+export default function ProfilePage() {
+  return (
+    <div className="max-w-7xl mx-auto py-12">
+      <ProfileCard />
+      <ProfileItems />
+    </div>
+  );
+}
