55c51307d63df2565994d47290779cef779e3472
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-08-24 23:18:17 +0300

    removed empty className atrributes

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,8 +4,8 @@ import ProfileLayout from "../components/Features/Profile/ProfileLayout";
 
 export default function ProfilePage() {
   return (
-    <main className="">
-      <ProfileLayout className="" />
+    <main>
+      <ProfileLayout />
     </main>
   );
 }
