96ebee83a2c640f6f75eb5fe24b97dacda60e255
IbadRehman360 <ibadhashim4@gmail.com>
2023-08-27 19:44:54 +0500

    created Message Page, set it's route added tons of styling.

---
+++ src/pages/MessagePage.jsx
@@ -0,0 +1,9 @@
+import MessageComponent from "../components/Features/MessagePage/MessageComponent";
+
+export default function MessagePage() {
+  return (
+    <div>
+      <MessageComponent />
+    </div>
+  );
+}
