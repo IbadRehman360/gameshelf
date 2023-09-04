b5bf4c75439a2ded6eb47f25490ece09c44ae93a
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-04 05:07:23 +0400

    Added edit image feature to the header background

--- src/components/Features/Profile/ProfileHeaderBg.jsx
+++ src/components/Features/Profile/ProfileHeaderBg.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiUpload } from "react-icons/hi";
+import { BsCardImage } from "react-icons/bs";
 
 export default function ProfileHeaderBg() {
+  const [showImageEditBtn, setShowImageEditBtn] = useState(false);
+
   return (
-    <section>
+    <section className="relative">
+      {showImageEditBtn && (
+        <div className="absolute z-50 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] ">
+          <button className="p-4 bg-slate-500 rounded-xl flex flex-col gap-2"
+           onMouseEnter={(e) => setShowImageEditBtn(true)}
+           onMouseLeave={(e) => setShowImageEditBtn(false)}
+          >
+            <BsCardImage className="w-full" color="white" size="1.3rem" />
+            <span className="text-white text-xs">Edit Image</span>
+          </button>
+        </div>
+      )}
       <div
-        className="w-full object-cover h-[240px] bg-center bg-cover bg-no-repeat sm:bg-cover "
+        className={`w-full ${showImageEditBtn && "opacity-70"} hover:opacity-70 object-cover h-[240px] bg-center bg-cover bg-no-repeat sm:bg-cover`}
         style={{ backgroundImage: 'url("/ProfileBanner.png")' }}
+        onMouseEnter={(e) => setShowImageEditBtn(true)}
+        onMouseLeave={(e) => setShowImageEditBtn(false)}
       >
         <span
           id="blackOverlay"
