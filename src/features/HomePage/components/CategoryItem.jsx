1ad460384c7b7fef66f0670dd5979259e655db23
0xJordan <ahmedjamlan12@gmail.com>
2023-09-14 10:23:29 +0400

    Implemented category name rendering

--- src/features/HomePage/components/CategoryItem.jsx
+++ src/features/HomePage/components/CategoryItem.jsx
@@ -1,4 +1,7 @@
 export default function categoryItem({ category, index }) {
+  function renderName(){
+    return category.name.split("-").join(" ")
+  }
   return (
     <div className="text-center h-12 w-11  lg:h-16    md:w-14 xl:h-[5.6rem] md:mt-2.5 md:mb-4 lg:mb-0   xl:w-[70px] flex flex-col items-center gap-2">
       <div
@@ -9,8 +12,8 @@ export default function categoryItem({ category, index }) {
           src={`/home/catergoriesBarImages/Navigation${index + 1}.webp`}
         />
       </div>
-      <p className="text-[11px] sm:text-[14px] md:text-[0.8rem] xl:text-[0.95rem] text-black whitespace-nowrap font-semibold">
-        {category.name}
+      <p className="capitalize text-[11px] sm:text-[14px] md:text-[0.8rem] xl:text-[0.95rem] text-black whitespace-nowrap font-semibold">
+        {renderName()}
       </p>
     </div>
   );
