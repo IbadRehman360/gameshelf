784e4c2a0b0b994e3c6c1899642b3d460870be69
Ibad ur Rehman <ibadhashim4@gmail.com>
2023-10-02 16:40:49 +0500

    made some changes

--- src/features/GameProducts/components/GameProducts.jsx
+++ src/features/GameProducts/components/GameProducts.jsx
@@ -39,7 +39,14 @@ export default function GameProducts() {
   if (isGameLoading || !isGames) {
     return (
       <div className="flex flex-col items-center justify-center py-16">
-        <Spinner className="h-20 w-20 text-red-500" />;
+        <div
+          class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>{" "}
       </div>
     );
   }
