ffde6f92e13023cf34c38c81a1855921b044f414
Ibad ur Rehman <ibadhashim4@gmail.com>
2023-10-04 18:41:59 +0500

    almost all the main features are completed, game product, profile feature, dashboard etc

--- src/features/HomePage/components/TrendingVideoGame.jsx
+++ src/features/HomePage/components/TrendingVideoGame.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function TrendingVideoGame({ game }) {
   return (
-    <Link>
+    <Link to={"/dashboard"}>
       <div className="relative flex flex-col bg-[#fdfdfd] px-1 sm:px-1 md:p-1 lg:p-4">
         <img src={game.src} alt="giftcardIcon" />
         <p className="absolute right-3 top-2 rounded-md bg-gray-50/25 bg-gradient-to-b p-[4px] text-center text-[10px] text-white shadow-2xl sm:top-3 sm:p-[3px] sm:text-[11px] md:right-4 md:p-[4px] md:text-[13px] lg:right-7 lg:top-7 lg:p-[6px] lg:text-[0.8erm]">
@@ -13,5 +13,5 @@ export default function TrendingVideoGame({ game }) {
         </span>
       </div>
     </Link>
-  )
+  );
 }
