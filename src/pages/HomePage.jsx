fde6a598910e93f31f60fea561166aca7729bb5c
Ibad ur Rehman <ibadhashim4@gmail.com>
2023-10-11 22:58:07 +0500

    Finalize v1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -72,7 +72,7 @@ export default function HomePage() {
         {!session ? (
           <div
             className="fixed bottom-0  py-2 bg-white w-full sm:hidden flex"
-            style={{ boxShadow: "0px -1px 2px rgba(0, 0, 0, 0.2)" }}
+            style={{ boxShadow: "0px -1px 0px rgba(0, 0, 0, 0.1)" }}
           >
             <Link
               to={"login"}
